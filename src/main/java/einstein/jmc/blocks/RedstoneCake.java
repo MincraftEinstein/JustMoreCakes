8475451be6771cf58c498ae13ec6203367c165c4
Seth Van Meter <SethVM0178@gmail.com>
2021-02-23 22:02:02 -0500

    Redstone cake now outputs a signal based on number of bites

--- src/main/java/einstein/jmc/blocks/RedstoneCake.java
+++ src/main/java/einstein/jmc/blocks/RedstoneCake.java
@@ -6,7 +6,9 @@ import einstein.einsteins_library.blocks.CakeBlockBase;
 import einstein.jmc.init.ModConfigs.ModClientConfigs;
 import net.minecraft.block.BlockState;
 import net.minecraft.particles.RedstoneParticleData;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -17,15 +19,25 @@ public class RedstoneCake extends CakeBlockBase {
 		super(properties);
 	}
 
+	@Override
+	public boolean canProvidePower(BlockState state) {
+		return true;
+	}
+
+	@Override
+	public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+		return 7 - blockState.get(BITES);
+	}
+
 	@OnlyIn(Dist.CLIENT)
 	public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
-		if(ModClientConfigs.REDSTONE_CAKE_PARTICLES.get()) {
-			for(int i = 0; i < 2; ++i) {
-		        double d0 = pos.getX() + rand.nextDouble();
-		        double d1 = pos.getY() + rand.nextDouble() * 0.5D + 0.25D;
-		        double d2 = pos.getZ() + rand.nextDouble();
+		if (ModClientConfigs.REDSTONE_CAKE_PARTICLES.get()) {
+			for (int i = 0; i < 2; ++i) {
+				double d0 = pos.getX() + rand.nextDouble();
+				double d1 = pos.getY() + rand.nextDouble() * 0.5D + 0.25D;
+				double d2 = pos.getZ() + rand.nextDouble();
 				worldIn.addParticle(RedstoneParticleData.REDSTONE_DUST, d0, d1, d2, 0.0D, 0.0D, 0.0D);
-			}	
+			}
 		}
 	}
 }
