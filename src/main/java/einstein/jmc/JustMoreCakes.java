eebf9912b9e5f909659965f9c8c3a5bf41de2c84
Seth Van Meter <70532030+MincraftEinstein@users.noreply.github.com>
2022-07-15 19:52:29 -0400

    Cleaned up JustMoreCakes.java

--- src/main/java/einstein/jmc/JustMoreCakes.java
+++ src/main/java/einstein/jmc/JustMoreCakes.java
@@ -2,8 +2,13 @@ package einstein.jmc;
 
 import com.mojang.datafixers.util.Pair;
 import cpw.mods.modlauncher.api.INameMappingService.Domain;
+import einstein.jmc.client.gui.screens.inventory.CakeOvenScreen;
 import einstein.jmc.init.*;
 import einstein.jmc.util.EventHandler;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.*;
 import net.minecraft.resources.ResourceLocation;
@@ -11,22 +16,29 @@ import net.minecraft.world.entity.ai.behavior.GiveGiftToHero;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.levelgen.structure.pools.LegacySinglePoolElement;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.coremod.api.ASMAPI;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.ParallelDispatchEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.MissingMappingsEvent;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.util.List;
@@ -38,14 +50,12 @@ public class JustMoreCakes {
     public static final String MODID = "jmc";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final JMCTab JMC_TAB = new JMCTab(CreativeModeTab.TABS.length, "jmc_tab");
-    public static JustMoreCakes instance;
-    private Field maxItemStackSize;
-    
+
     public JustMoreCakes() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::setup);
-        modEventBus.addListener(this::doClientStuff);
-//        modEventBus.addGenericListener(RecipeSerializer.class, this::registerRecipeType);
+        modEventBus.register(this);
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
         ModItems.ITEMS.register(modEventBus);
         ModBlocks.init(modEventBus);
         ModBlockEntityTypes.BLOCK_ENTITIES.register(modEventBus);
@@ -56,132 +66,119 @@ public class JustMoreCakes {
         ModVillagers.PROFESSIONS.register(modEventBus);
         ModPotions.MOB_EFFECTS.register(modEventBus);
         ModPotions.POTIONS.register(modEventBus);
-        JustMoreCakes.instance = this;
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new EventHandler());
+        MinecraftForge.EVENT_BUS.addListener(this::missingMappings);
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ModServerConfigs.SERVERSPEC);
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModClientConfigs.CLIENTSPEC);
-        accessStackSize();
-        if (maxItemStackSize != null) {
-        	modEventBus.addListener(this::resizeCakeStack);
-        }
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ModClientConfigs.CLIENTSPEC);
     }
-    
-	private void setup(final FMLCommonSetupEvent event) {
-		registerVillageBuilding("houses/plains_bakery_1");
-		registerVillageBuilding("houses/plains_bakery_2");
-		registerVillageBuilding("houses/desert_bakery_1");
-		registerVillageBuilding("houses/savanna_bakery_1");
-		registerVillageBuilding("houses/snowy_bakery_1");
-		registerVillageBuilding("houses/snowy_bakery_2");
-		registerVillageBuilding("houses/taiga_bakery_1");
-		GiveGiftToHero.GIFTS.put(ModVillagers.CAKE_BAKER.get(), new ResourceLocation(MODID, "gameplay/hero_of_the_village/cake_baker_gift"));
+
+    void commonSetup(final FMLCommonSetupEvent event) {
+        registerVillageBuilding("plains", "bakery_1");
+        registerVillageBuilding("plains", "bakery_2");
+        registerVillageBuilding("desert", "bakery_1");
+        registerVillageBuilding("savanna", "bakery_1");
+        registerVillageBuilding("snowy", "bakery_1");
+        registerVillageBuilding("snowy", "bakery_2");
+        registerVillageBuilding("taiga", "bakery_1");
+        GiveGiftToHero.GIFTS.put(ModVillagers.CAKE_BAKER.get(), new ResourceLocation(MODID, "gameplay/hero_of_the_village/cake_baker_gift"));
+        resizeCakeStack();
     }
-	
-	@SuppressWarnings("unchecked")
-	public static void registerVillageBuilding(final String location) {
-    	PlainVillagePools.bootstrap();
-    	DesertVillagePools.bootstrap();
-    	SavannaVillagePools.bootstrap();
-    	SnowyVillagePools.bootstrap();
-    	TaigaVillagePools.bootstrap();
-		for (String biome : new String[] { "plains", "snowy", "savanna", "desert", "taiga" }) {
-        	final StructureTemplatePool pattern = BuiltinRegistries.TEMPLATE_POOL.get(new ResourceLocation("minecraft:village/" + biome + "/houses"));
-            if (pattern == null) {
-                return;
-            }
-            final Function<StructureTemplatePool.Projection, LegacySinglePoolElement> element = StructurePoolElement.legacy(MODID + ":village/" + biome + "/" + location, ProcessorLists.MOSSIFY_10_PERCENT);
-            final StructurePoolElement structure = element.apply(StructureTemplatePool.Projection.RIGID);
-            try {
-                final String name = ASMAPI.mapField("f_210560_");
-                final Field field = StructureTemplatePool.class.getDeclaredField(name);
-                field.setAccessible(true);
-                final String name2 = ASMAPI.mapField("f_210559_");
-                final Field field2 = StructureTemplatePool.class.getDeclaredField(name2);
-                field2.setAccessible(true);
-    			final List<StructurePoolElement> list = (List<StructurePoolElement>)field.get(pattern);
-                for (int i = 0; i < 1; ++i) {
-                    list.add(structure);
-                }
-                final List<Pair<StructurePoolElement, Integer>> list2 = (List<Pair<StructurePoolElement, Integer>>)field2.get(pattern);
-                list2.add(Pair.of(structure, 1));
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-    	}
-    }
-	
-	private void doClientStuff(final ParallelDispatchEvent event) {
-		event.enqueueWork(() -> {
-			ModPotions.registerPotionRecipes();
-		});
-	}
-	
-//	private void registerRecipeType(RegistryEvent.Register<RecipeSerializer<?>> event) {
-//		Registry.register(Registry.RECIPE_TYPE, ModRecipes.CAKE_OVEN_RECIPE.toString(), ModRecipes.CAKE_OVEN_RECIPE);
-//	}
-	
-//	@Nullable
-//	private static Block missingBlock(String name) {
-//		switch (name) {
-//		case "cheese_cake":
-//			return ModBlocks.CHEESECAKE.get();
-//		case "triple_decker_cake":
-//			return ModBlocks.THREE_TIERED_CAKE.get();
-//		}
-//		return null;
-//	}
-//
-//	@SubscribeEvent
-//	void missingItems(final MissingMappings<Item> event) {
-//		handleMissingMappings(event, MODID, name -> {
-//			switch (name) {
-//			case "cheese_cake":
-//				return ModBlocks.CHEESECAKE.get().asItem();
-//			case "triple_decker_cake":
-//				return ModBlocks.THREE_TIERED_CAKE.get().asItem();
-//			}
-//
-//			ItemLike block = missingBlock(name);
-//			return block == null ? null : block.asItem();
-//		});
-//	}
-//
-//	@SubscribeEvent
-//	void missingBlocks(final MissingMappings<Block> event) {
-//		handleMissingMappings(event, MODID, JustMoreCakes::missingBlock);
-//	}
-	
-//	public static <T extends IForgeRegistryEntry<T>> void handleMissingMappings(MissingMappings<T> event, String modID, Function<String, T> handler) {
-//		for (Mapping<T> mapping : event.getAllMappings()) {
-//			if (modID.equals(mapping.key.getNamespace())) {
-//				@Nullable
-//				T value = handler.apply(mapping.key.getPath());
-//				if (value != null) {
-//					mapping.remap(value);
-//				}
-//			}
-//		}
-//	}
-	
-    private void accessStackSize() {
+
+    void resizeCakeStack() {
+        Field maxItemStackSize = null;
+
         try {
             final Field field = Item.class.getDeclaredField(ObfuscationReflectionHelper.remapName(Domain.FIELD, "f_41478_"));
             field.setAccessible(true);
             maxItemStackSize = field;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             LOGGER.catching(Level.WARN, e);
         }
+
+        if (maxItemStackSize != null) {
+            try {
+                maxItemStackSize.setInt(Items.CAKE, 64);
+            } catch (Exception e) {
+                LOGGER.catching(Level.WARN, e);
+            }
+        }
     }
-    
-    private void resizeCakeStack(FMLCommonSetupEvent event) {
-		try {
-			maxItemStackSize.setInt(Items.CAKE, 64);
-		}
-		catch (Exception e) {
-			LOGGER.catching(Level.WARN, e);
-		}
-	}
+
+    @SuppressWarnings("unchecked")
+    void registerVillageBuilding(String biome, String name) {
+        PlainVillagePools.bootstrap();
+        DesertVillagePools.bootstrap();
+        SavannaVillagePools.bootstrap();
+        SnowyVillagePools.bootstrap();
+        TaigaVillagePools.bootstrap();
+
+        final StructureTemplatePool templatePool = BuiltinRegistries.TEMPLATE_POOL.get(new ResourceLocation("minecraft:village/" + biome + "/houses"));
+
+        if (templatePool == null) {
+            LOGGER.warn("Failed to register " + biome + " village building");
+            return;
+        }
+
+        final StructurePoolElement structure = StructurePoolElement.legacy(MODID + ":village/" + biome + "/houses/" + biome + "_" + name, ProcessorLists.MOSSIFY_10_PERCENT).apply(StructureTemplatePool.Projection.RIGID);
+
+        try {
+            final Field templatesField = StructureTemplatePool.class.getDeclaredField(ASMAPI.mapField("f_210560_"));
+            templatesField.setAccessible(true);
+            final ObjectArrayList<StructurePoolElement> templates = (ObjectArrayList<StructurePoolElement>) templatesField.get(templatePool);
+            templates.add(structure);
+
+            final Field rawTemplatesField = StructureTemplatePool.class.getDeclaredField(ASMAPI.mapField("f_210559_"));
+            rawTemplatesField.setAccessible(true);
+            final List<Pair<StructurePoolElement, Integer>> rawTemplates = (List<Pair<StructurePoolElement, Integer>>) rawTemplatesField.get(templatePool);
+            rawTemplates.add(Pair.of(structure, 1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SubscribeEvent
+    void onParallelDispatch(final ParallelDispatchEvent event) {
+        event.enqueueWork(ModPotions::registerPotionRecipes);
+    }
+
+    void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.RED_MUSHROOM_CAKE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.BROWN_MUSHROOM_CAKE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CHORUS_CAKE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CRIMSON_FUNGUS_CAKE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.ENCASING_ICE.get(), RenderType.translucent());
+
+        MenuScreens.register(ModMenuTypes.CAKE_OVEN.get(), CakeOvenScreen::new);
+    }
+
+    void missingMappings(MissingMappingsEvent event) {
+        handleMissingMappings(event, ForgeRegistries.ITEMS, (name) -> missingBlock(name).asItem());
+        handleMissingMappings(event, ForgeRegistries.BLOCKS, this::missingBlock);
+    }
+
+    @Nullable
+    private Block missingBlock(String name) {
+        switch (name) {
+            case "cheese_cake":
+                return ModBlocks.CHEESECAKE.get();
+            case "triple_decker_cake":
+                return ModBlocks.THREE_TIERED_CAKE.get();
+        }
+        return null;
+    }
+
+    private <T> void handleMissingMappings(MissingMappingsEvent event, IForgeRegistry registry, Function<String, T> function) {
+        List<MissingMappingsEvent.Mapping<T>> mappings = event.getMappings(registry.getRegistryKey(), MODID);
+
+        for (MissingMappingsEvent.Mapping mapping : mappings) {
+            T value = function.apply(mapping.getKey().getPath());
+            if (value != null) {
+                mapping.remap(value);
+            } else {
+                LOGGER.info("Failed to remap (" + mapping.getKey().toString() + ") of registry (" + registry.getRegistryName() + ")");
+                mapping.fail();
+            }
+        }
+    }
 }
