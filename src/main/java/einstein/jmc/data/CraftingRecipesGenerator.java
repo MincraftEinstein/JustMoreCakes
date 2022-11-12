a29429e095cd408f992bcaf97cbb2d27a74e1a7d
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2022-11-11 22:46:20 -0500

    Changed cupcake recipe

--- src/main/java/einstein/jmc/data/CraftingRecipesGenerator.java
+++ src/main/java/einstein/jmc/data/CraftingRecipesGenerator.java
@@ -9,6 +9,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -20,52 +21,49 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class CraftingRecipesGenerator extends RecipeProvider {
 
-	public CraftingRecipesGenerator(DataGenerator generator) {
-		super(generator);
-	}
-	
-	@Override
-	protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
-		String has = "has_item";
-		ShapedRecipeBuilder.shaped(ModItems.CUPCAKE.get())
-			.pattern(" # ")
-			.pattern("$~$")
-			.pattern(" @ ")
-			.define('#', Items.MILK_BUCKET)
-			.define('$', Items.SUGAR)
-			.define('~', Items.EGG)
-			.define('@', Items.WHEAT)
-			.unlockedBy(has, has(Items.EGG))
-			.save(consumer, ModItems.CUPCAKE.getId());
-		
-		ShapedRecipeBuilder.shaped(ModBlocks.THREE_TIERED_CAKE.get())
-			.pattern("#")
-			.pattern("#")
-			.pattern("#")
-			.define('#', Items.CAKE)
-			.unlockedBy(has, has(Items.CAKE))
-			.save(consumer, ModBlocks.THREE_TIERED_CAKE.getId());
-		
-		ShapedRecipeBuilder.shaped(ModItems.CHEESE.get(), 3)
-			.pattern("###")
-			.pattern("###")
-			.define('#', Items.MILK_BUCKET)
-			.unlockedBy(has, has(Items.MILK_BUCKET))
-			.save(consumer, ModItems.CHEESE.getId());
-		
-		ShapedRecipeBuilder.shaped(ModBlocks.CAKE_OVEN.get().asItem())
-			.pattern("###")
-			.pattern("#$#")
-			.pattern("~~~")
-			.define('#', Items.BRICK)
-			.define('$', Blocks.FURNACE)
-			.define('~', Blocks.SMOOTH_STONE)
-			.unlockedBy(has, has(Blocks.SMOOTH_STONE))
-			.save(consumer, ModBlocks.CAKE_OVEN.getId());
-	}
+    public CraftingRecipesGenerator(DataGenerator generator) {
+        super(generator);
+    }
 
-	@Override
-	public String getName() {
-		return "JustMoreCakes crafting recipes";
-	}
+    @Override
+    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
+        String has = "has_item";
+        ShapelessRecipeBuilder.shapeless(ModBlocks.CUPCAKE.get(), 4)
+                .requires(Items.EGG)
+                .requires(Items.MILK_BUCKET)
+                .requires(Items.WHEAT)
+                .requires(Items.SUGAR)
+                .unlockedBy(has, has(Items.EGG))
+                .save(consumer, ModBlocks.CUPCAKE.getId());
+
+        ShapedRecipeBuilder.shaped(ModBlocks.THREE_TIERED_CAKE.get())
+                .pattern("#")
+                .pattern("#")
+                .pattern("#")
+                .define('#', Items.CAKE)
+                .unlockedBy(has, has(Items.CAKE))
+                .save(consumer, ModBlocks.THREE_TIERED_CAKE.getId());
+
+        ShapedRecipeBuilder.shaped(ModItems.CHEESE.get(), 3)
+                .pattern("###")
+                .pattern("###")
+                .define('#', Items.MILK_BUCKET)
+                .unlockedBy(has, has(Items.MILK_BUCKET))
+                .save(consumer, ModItems.CHEESE.getId());
+
+        ShapedRecipeBuilder.shaped(ModBlocks.CAKE_OVEN.get().asItem())
+                .pattern("###")
+                .pattern("#$#")
+                .pattern("~~~")
+                .define('#', Items.BRICK)
+                .define('$', Blocks.FURNACE)
+                .define('~', Blocks.SMOOTH_STONE)
+                .unlockedBy(has, has(Blocks.SMOOTH_STONE))
+                .save(consumer, ModBlocks.CAKE_OVEN.getId());
+    }
+
+    @Override
+    public String getName() {
+        return "JustMoreCakes crafting recipes";
+    }
 }
