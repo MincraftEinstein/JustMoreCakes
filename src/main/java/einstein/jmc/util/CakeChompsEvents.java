57ec7c67a90f448af4ac25483206237adb018566
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2022-11-14 22:23:03 -0500

    Fixed CakeChomps particles spawning when clicking a cake if not hungry

--- src/main/java/einstein/jmc/util/CakeChompsEvents.java
+++ src/main/java/einstein/jmc/util/CakeChompsEvents.java
@@ -43,45 +43,47 @@ public class CakeChompsEvents {
 		BlockState state = level.getBlockState(pos);
 		Block block = state.getBlock();
 
-		if (block instanceof BaseCakeBlock || block instanceof BaseCandleCakeBlock) {
-			if (!player.getItemInHand(hand).is(ItemTags.CANDLES) || !(block instanceof BaseCakeBlock) || !state.getOptionalValue(BaseCakeBlock.BITES).map(value -> value == 0).orElse(false)) {
+		if (player.canEat(false)) {
+			if (block instanceof BaseCakeBlock || block instanceof BaseCandleCakeBlock) {
+				if (!player.getItemInHand(hand).is(ItemTags.CANDLES) || !(block instanceof BaseCakeBlock) || !state.getOptionalValue(BaseCakeBlock.BITES).map(value -> value == 0).orElse(false)) {
 
-				UseOnContext useoncontext = new UseOnContext(player, hand, hitResult);
-				ItemStack stack = player.getItemInHand(hand);
+					UseOnContext useoncontext = new UseOnContext(player, hand, hitResult);
+					ItemStack stack = player.getItemInHand(hand);
 
-				if (event.getUseItem() != Result.DENY) {
-					InteractionResult interact = stack.onItemUseFirst(useoncontext);
+					if (event.getUseItem() != Result.DENY) {
+						InteractionResult interact = stack.onItemUseFirst(useoncontext);
 
-					if (interact != InteractionResult.PASS) {
-						return;
-					}
-				}
-
-				boolean flag = !player.getMainHandItem().isEmpty() || !player.getOffhandItem().isEmpty();
-				boolean flag1 = player.isSecondaryUseActive() && flag && (!player.getMainHandItem().doesSneakBypassUse(player.getLevel(), pos, player) || !player.getOffhandItem().doesSneakBypassUse(player.getLevel(), pos, player));
-				if (event.getUseBlock() == Result.ALLOW || event.getUseBlock() != Result.DENY && !flag1) {
-					ItemStack blockStack = block.getCloneItemStack(level, pos, state);
-
-					for (int i = 0; i < 16; ++i) {
-						Vec3 vec3 = new Vec3((RANDOM.nextFloat() - 0.5F) * 0.1F, Math.random() * 0.1F + 0.1F, 0);
-						vec3 = vec3.xRot(-player.getXRot() * 0.017453292F);
-						vec3 = vec3.yRot(-player.getYRot() * 0.017453292F);
-						double d0 = (-RANDOM.nextFloat()) * 0.6F - 0.3F;
-						Vec3 vec31 = new Vec3((RANDOM.nextFloat() - 0.5F) * 0.3F, d0, 0.6F);
-						vec31 = vec31.xRot(-player.getXRot() * 0.017453292F);
-						vec31 = vec31.yRot(-player.getYRot() * 0.017453292F);
-						vec31 = vec31.add(player.getX(), player.getEyeY(), player.getZ());
-						ParticleOptions particle = new ItemParticleOption(ParticleTypes.ITEM, blockStack);
-						Level var15 = player.level;
-						if (var15 instanceof ServerLevel server) {
-							server.sendParticles(particle, vec31.x, vec31.y, vec31.z, 1, vec3.x, vec3.y + 0.05F, vec3.z, 0);
-						}
-						else {
-							level.addParticle(particle, vec31.x, vec31.y, vec31.z, vec3.x, vec3.y + 0.05, vec3.z);
+						if (interact != InteractionResult.PASS) {
+							return;
 						}
 					}
 
-					player.playSound(player.getEatingSound(blockStack), 0.5F + 0.5F * RANDOM.nextInt(2), (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1.0F);
+					boolean flag = !player.getMainHandItem().isEmpty() || !player.getOffhandItem().isEmpty();
+					boolean flag1 = player.isSecondaryUseActive() && flag && (!player.getMainHandItem().doesSneakBypassUse(player.getLevel(), pos, player) || !player.getOffhandItem().doesSneakBypassUse(player.getLevel(), pos, player));
+					if (event.getUseBlock() == Result.ALLOW || event.getUseBlock() != Result.DENY && !flag1) {
+						ItemStack blockStack = block.getCloneItemStack(level, pos, state);
+
+						for (int i = 0; i < 16; ++i) {
+							Vec3 vec3 = new Vec3((RANDOM.nextFloat() - 0.5F) * 0.1F, Math.random() * 0.1F + 0.1F, 0);
+							vec3 = vec3.xRot(-player.getXRot() * 0.017453292F);
+							vec3 = vec3.yRot(-player.getYRot() * 0.017453292F);
+							double d0 = (-RANDOM.nextFloat()) * 0.6F - 0.3F;
+							Vec3 vec31 = new Vec3((RANDOM.nextFloat() - 0.5F) * 0.3F, d0, 0.6F);
+							vec31 = vec31.xRot(-player.getXRot() * 0.017453292F);
+							vec31 = vec31.yRot(-player.getYRot() * 0.017453292F);
+							vec31 = vec31.add(player.getX(), player.getEyeY(), player.getZ());
+							ParticleOptions particle = new ItemParticleOption(ParticleTypes.ITEM, blockStack);
+							Level var15 = player.level;
+							if (var15 instanceof ServerLevel server) {
+								server.sendParticles(particle, vec31.x, vec31.y, vec31.z, 1, vec3.x, vec3.y + 0.05F, vec3.z, 0);
+							}
+							else {
+								level.addParticle(particle, vec31.x, vec31.y, vec31.z, vec3.x, vec3.y + 0.05, vec3.z);
+							}
+						}
+
+						player.playSound(player.getEatingSound(blockStack), 0.5F + 0.5F * RANDOM.nextInt(2), (RANDOM.nextFloat() - RANDOM.nextFloat()) * 0.2F + 1.0F);
+					}
 				}
 			}
 		}
