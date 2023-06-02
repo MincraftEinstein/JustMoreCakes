ce755cb9a7daf5d2af91c578051afee22fc69f39
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-06-01 22:37:10 -0400

    Filled "cake_spatula_usable" tag

--- Fabric/src/main/java/einstein/jmc/data/providers/ModBlockTagsProvider.java
+++ Fabric/src/main/java/einstein/jmc/data/providers/ModBlockTagsProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -20,7 +21,6 @@ public class ModBlockTagsProvider extends FabricTagProvider.BlockTagProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.CAKE_OVEN.get(), ModBlocks.OBSIDIAN_CAKE.get());
-        getOrCreateTagBuilder(ModBlockTags.CAKE_SPATULA_USABLE).add(ModBlocks.CHOCOLATE_CAKE.get());
         CakeBuilder.BUILDER_BY_CAKE.forEach((cake, cakeBuilder) -> {
             getOrCreateTagBuilder(ModBlockTags.CAKES).add(cake.get());
             cakeBuilder.getCandleCakeByCandle().forEach((candle, candleCake) -> {
@@ -30,5 +30,11 @@ public class ModBlockTagsProvider extends FabricTagProvider.BlockTagProvider {
 
         getOrCreateTagBuilder(ModBlockTags.C_CAKES).addTag(ModBlockTags.CAKES);
         getOrCreateTagBuilder(ModBlockTags.C_CANDLE_CAKES).addTag(ModBlockTags.CANDLE_CAKES);
+        getOrCreateTagBuilder(ModBlockTags.CAKE_SPATULA_USABLE).add(Blocks.CAKE, Blocks.CANDLE_CAKE, Blocks.WHITE_CANDLE_CAKE,
+                        Blocks.ORANGE_CANDLE_CAKE, Blocks.MAGENTA_CANDLE_CAKE, Blocks.LIGHT_BLUE_CANDLE_CAKE,
+                        Blocks.YELLOW_CANDLE_CAKE, Blocks.LIME_CANDLE_CAKE, Blocks.PINK_CANDLE_CAKE, Blocks.GRAY_CANDLE_CAKE,
+                        Blocks.LIGHT_GRAY_CANDLE_CAKE, Blocks.CYAN_CANDLE_CAKE, Blocks.PURPLE_CANDLE_CAKE, Blocks.BLUE_CANDLE_CAKE,
+                        Blocks.BROWN_CANDLE_CAKE, Blocks.GREEN_CANDLE_CAKE, Blocks.RED_CANDLE_CAKE, Blocks.BLACK_CANDLE_CAKE)
+                .addTag(ModBlockTags.C_CAKES).addTag(ModBlockTags.C_CANDLE_CAKES);
     }
 }
--- Forge/src/main/java/einstein/jmc/data/providers/ModBlockTagsProvider.java
+++ Forge/src/main/java/einstein/jmc/data/providers/ModBlockTagsProvider.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -26,7 +27,6 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.CAKE_OVEN.get(), ModBlocks.OBSIDIAN_CAKE.get());
-        tag(ModBlockTags.CAKE_SPATULA_USABLE).add(ModBlocks.CHOCOLATE_CAKE.get());
         CakeBuilder.BUILDER_BY_CAKE.forEach((cake, cakeBuilder) -> {
             tag(ModBlockTags.CAKES).add(cake.get());
             cakeBuilder.getCandleCakeByCandle().forEach((candle, candleCake) -> {
@@ -38,5 +38,11 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
         tag(ModBlockTags.C_CANDLE_CAKES).addTag(ModBlockTags.CANDLE_CAKES);
         tag(F_CAKES).addTag(ModBlockTags.CAKES);
         tag(F_CANDLE_CAKES).addTag(ModBlockTags.CANDLE_CAKES);
+        tag(ModBlockTags.CAKE_SPATULA_USABLE).add(Blocks.CAKE, Blocks.CANDLE_CAKE, Blocks.WHITE_CANDLE_CAKE,
+                        Blocks.ORANGE_CANDLE_CAKE, Blocks.MAGENTA_CANDLE_CAKE, Blocks.LIGHT_BLUE_CANDLE_CAKE,
+                        Blocks.YELLOW_CANDLE_CAKE, Blocks.LIME_CANDLE_CAKE, Blocks.PINK_CANDLE_CAKE, Blocks.GRAY_CANDLE_CAKE,
+                        Blocks.LIGHT_GRAY_CANDLE_CAKE, Blocks.CYAN_CANDLE_CAKE, Blocks.PURPLE_CANDLE_CAKE, Blocks.BLUE_CANDLE_CAKE,
+                        Blocks.BROWN_CANDLE_CAKE, Blocks.GREEN_CANDLE_CAKE, Blocks.RED_CANDLE_CAKE, Blocks.BLACK_CANDLE_CAKE)
+                .addTag(F_CAKES).addTag(F_CANDLE_CAKES);
     }
 }
