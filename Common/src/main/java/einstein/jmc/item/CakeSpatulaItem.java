c075392e76eaed60355fa301b0ab78f7cb6d7786
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-06-01 20:24:01 -0400

    Finished cake spatula functionality

--- Common/src/main/java/einstein/jmc/item/CakeSpatulaItem.java
+++ Common/src/main/java/einstein/jmc/item/CakeSpatulaItem.java
@@ -1,17 +1,25 @@
 package einstein.jmc.item;
 
 import einstein.jmc.data.providers.ModBlockTags;
+import net.minecraft.ChatFormatting;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stat;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CakeSpatulaItem extends Item {
 
@@ -20,22 +28,31 @@ public class CakeSpatulaItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        if (player.isShiftKeyDown()) {
-            BlockHitResult result = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
-            BlockPos lookPos = result.getBlockPos();
-            BlockState state = level.getBlockState(lookPos);
-            Block block = state.getBlock();
+    public InteractionResult useOn(UseOnContext context) {
+        Player player = context.getPlayer();
+        if (player != null && player.isShiftKeyDown()) {
+            Level level = context.getLevel();
+            BlockPos pos = context.getClickedPos();
+            BlockState state = level.getBlockState(pos);
+            ItemStack stack = context.getItemInHand();
             if (state.is(ModBlockTags.CAKE_SPATULA_USABLE)) {
-//                if (block instanceof BaseCakeBlock || block instanceof CakeBlock) {
-//                    if () {
-//
-//                    }
-//                }
-                level.destroyBlock(lookPos, true);
-                return InteractionResultHolder.success(player.getItemInHand(hand));
+                if (!level.isClientSide) {
+                    level.destroyBlock(pos, !player.isCreative());
+                    stack.hurtAndBreak(1, player, entity -> entity.broadcastBreakEvent(context.getHand()));
+                    CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, pos, stack);
+                }
+
+                level.gameEvent(GameEvent.BLOCK_DESTROY, pos, GameEvent.Context.of(player, state));
+                player.awardStat(Stats.BLOCK_MINED.get(state.getBlock()));
+                return InteractionResult.SUCCESS;
             }
         }
-        return super.use(level, player, hand);
+        return super.useOn(context);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, level, tooltip, flag);
+        tooltip.add(Component.translatable("item.jmc.cake_spatula.desc").withStyle(ChatFormatting.GRAY));
     }
 }
