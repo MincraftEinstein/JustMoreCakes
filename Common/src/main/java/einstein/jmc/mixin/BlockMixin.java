4a12c72f2fd44734e84d66bfbad473de2fa9bba0
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-06-27 23:18:02 -0400

    Blocks in "cake spatula usable" tag won't drop when mined with cake spatula

---
+++ Common/src/main/java/einstein/jmc/mixin/BlockMixin.java
@@ -0,0 +1,30 @@
+package einstein.jmc.mixin;
+
+import einstein.jmc.data.providers.ModBlockTags;
+import einstein.jmc.init.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static net.minecraft.world.level.block.Block.dropResources;
+
+@Mixin(Block.class)
+public abstract class BlockMixin {
+
+    @Redirect(method = "playerDestroy", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V"))
+    private void playerDestroy(BlockState state, Level level, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack) {
+        if (stack.is(ModItems.CAKE_SPATULA.get()) && state.is(ModBlockTags.CAKE_SPATULA_USABLE)) {
+            dropResources(state, level, pos, blockEntity, entity, ItemStack.EMPTY);
+        }
+        else {
+            dropResources(state, level, pos, blockEntity, entity, stack);
+        }
+    }
+}
