912ea60fc5d534579da4f038b4a80daf92fb5d9e
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-10-04 18:17:10 -0400

    Added config translation keys

--- Common/src/main/java/einstein/jmc/init/ModClientConfigs.java
+++ Common/src/main/java/einstein/jmc/init/ModClientConfigs.java
@@ -2,13 +2,21 @@ package einstein.jmc.init;
 
 import net.minecraftforge.common.ForgeConfigSpec;
 
+import static einstein.jmc.init.ModCommonConfigs.key;
+
 public class ModClientConfigs {
 
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder().comment("Particle effects emitted from cakes").push("Cake particles");
 
-    public static final ForgeConfigSpec.BooleanValue REDSTONE_CAKE_PARTICLES = BUILDER.define("redstoneCakeParticles", true);
-    public static final ForgeConfigSpec.BooleanValue ENDER_CAKE_PARTICLES = BUILDER.define("enderCakeParticles", true);
-    public static final ForgeConfigSpec.BooleanValue LAVA_CAKE_PARTICLES = BUILDER.define("lavaCakeParticles", false);
+    public static final ForgeConfigSpec.BooleanValue REDSTONE_CAKE_PARTICLES = BUILDER
+            .translation(key("config.jmc.redstone_cake_particles"))
+            .define("redstoneCakeParticles", true);
+    public static final ForgeConfigSpec.BooleanValue ENDER_CAKE_PARTICLES = BUILDER
+            .translation(key("config.jmc.ender_cake_particles"))
+            .define("enderCakeParticles", true);
+    public static final ForgeConfigSpec.BooleanValue LAVA_CAKE_PARTICLES = BUILDER
+            .translation(key("lava_cake_particles"))
+            .define("lavaCakeParticles", false);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
--- Common/src/main/java/einstein/jmc/init/ModCommonConfigs.java
+++ Common/src/main/java/einstein/jmc/init/ModCommonConfigs.java
@@ -1,29 +1,52 @@
 package einstein.jmc.init;
 
+import einstein.jmc.JustMoreCakes;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class ModCommonConfigs {
 
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ForgeConfigSpec.IntValue FIREY_CAKE_ON_FIRE_DUR = BUILDER.comment("How long the cake eater is set on fire for in ticks")
+    public static final ForgeConfigSpec.IntValue FIREY_CAKE_ON_FIRE_DUR = BUILDER
+            .comment("How long the cake eater is set on fire for in ticks")
+            .translation(key("firey_cake_fire_duration"))
             .defineInRange("fireyCakeOnFireDuration", 20, 0, 32000);
-    public static final ForgeConfigSpec.IntValue GLOWSTONE_CAKE_GLOWING_DUR = BUILDER.comment("How long mobs affected by the glowstone cake glow for in ticks")
+    public static final ForgeConfigSpec.IntValue GLOWSTONE_CAKE_GLOWING_DUR = BUILDER
+            .comment("How long mobs affected by the glowstone cake glow for in ticks")
+            .translation(key("glowstone_cake_glowing_duration"))
             .defineInRange("glowstoneCakeGlowingDuration", 85, 0, 32000);
-    public static final ForgeConfigSpec.DoubleValue CHORUS_CAKE_TELEPORT_RADIUS = BUILDER.comment("The radius around the player in which the they will be randomly teleported")
+    public static final ForgeConfigSpec.DoubleValue CHORUS_CAKE_TELEPORT_RADIUS = BUILDER
+            .comment("The radius around the player in which the they will be randomly teleported")
+            .translation(key("chorus_cake_teleport_radius"))
             .defineInRange("chorusCakeTeleportRadius", 10, 0, 10000D);
-    public static final ForgeConfigSpec.DoubleValue ENDER_CAKE_TELEPORT_RADIUS = BUILDER.comment("The radius around the player in which the they will be randomly teleported")
+    public static final ForgeConfigSpec.DoubleValue ENDER_CAKE_TELEPORT_RADIUS = BUILDER
+            .comment("The radius around the player in which the they will be randomly teleported")
+            .translation(key("ender_cake_teleport_radius"))
             .defineInRange("enderCakeTeleportRadius", 50, 0, 10000D);
-    public static final ForgeConfigSpec.BooleanValue EFFECTED_BY_REDSTONE = BUILDER.comment("Whether the TNT cake will explode when powered by Redstone")
+    public static final ForgeConfigSpec.BooleanValue EFFECTED_BY_REDSTONE = BUILDER
+            .comment("Whether the TNT cake will explode when powered by Redstone")
+            .translation(key("effected_by_redstone"))
             .define("effectedByRedstone", false);
-    public static final ForgeConfigSpec.IntValue TNT_CAKE_EXPLOSION_SIZE = BUILDER.comment("How big the explosion for TNT cake is")
+    public static final ForgeConfigSpec.IntValue TNT_CAKE_EXPLOSION_SIZE = BUILDER
+            .comment("How big the explosion for TNT cake is")
+            .translation(key("tnt_cake_explosion_size"))
             .defineInRange("tntCakeExplosionSize", 5, 1, 50);
-    public static final ForgeConfigSpec.IntValue ENCASING_ICE_MELT_SPEED = BUILDER.comment("How long it takes for encasing ice to melt. Larger numbers is slower")
+    public static final ForgeConfigSpec.IntValue ENCASING_ICE_MELT_SPEED = BUILDER
+            .comment("How long it takes for encasing ice to melt. Larger numbers is slower")
+            .translation(key("encasing_ice_melt_speed"))
             .defineInRange("encasingIceMeltSpeed", 1, 0, Integer.MAX_VALUE);
-    public static final ForgeConfigSpec.IntValue CAKE_BAKERY_GENERATION_WEIGHT = BUILDER.comment("How often do cake bakeries generate in villages. 0 to disable")
+    public static final ForgeConfigSpec.IntValue CAKE_BAKERY_GENERATION_WEIGHT = BUILDER
+            .comment("How often do cake bakeries generate in villages. 0 to disable")
+            .translation(key("cake_bakery_generation_weight"))
             .defineInRange("cakeBakeryGenerationWeight", 1, 0, 150);
-    public static final ForgeConfigSpec.IntValue GLOWSTONE_CAKE_EFFECT_RADIUS = BUILDER.comment("The radius in blocks at which the glowstone cake will effect mobs")
+    public static final ForgeConfigSpec.IntValue GLOWSTONE_CAKE_EFFECT_RADIUS = BUILDER
+            .comment("The radius in blocks at which the glowstone cake will effect mobs")
+            .translation(key("glowstone_cake_effect_radius"))
             .defineInRange("glowstoneCakeEffectRadius", 20, 0, 100);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    public static String key(String name) {
+        return "config." + JustMoreCakes.MOD_ID + "." + name;
+    }
 }
