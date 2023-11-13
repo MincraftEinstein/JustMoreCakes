2b8ab265aa3008f2642d79cda61d637b09947e5e
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-11-13 12:01:18 -0500

    Added Cake Stand to pickaxe mineable tag

--- fabric/src/main/java/einstein/jmc/data/packs/providers/ModBlockTagsProvider.java
+++ fabric/src/main/java/einstein/jmc/data/packs/providers/ModBlockTagsProvider.java
@@ -26,7 +26,7 @@ public class ModBlockTagsProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
-        getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.CAKE_OVEN.get(), ModBlocks.OBSIDIAN_CAKE.get());
+        getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.CAKE_OVEN.get(), ModBlocks.OBSIDIAN_CAKE.get(), ModBlocks.CAKE_STAND.get());
 
         Map<Supplier<BaseCakeBlock>, CakeBuilder> sortedCakes = Util.createValueSortedMap(CakeBuilder.BUILDER_BY_CAKE, Comparator.comparing(CakeBuilder::getCakeName));
 
