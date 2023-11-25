7017c5d78b6998a018fe69fb40f3902e60879b12
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-11-25 00:13:17 -0500

    Added Jade block provider for displaying cake effects

---
+++ common/src/main/java/einstein/jmc/compat/jade/CakeEffectsProvider.java
@@ -0,0 +1,72 @@
+package einstein.jmc.compat.jade;
+
+import einstein.jmc.JustMoreCakes;
+import einstein.jmc.block.CakeEffectsHolder;
+import einstein.jmc.block.cake.candle.BaseCandleCakeBlock;
+import einstein.jmc.data.cakeeffect.CakeEffects;
+import einstein.jmc.util.Util;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.StringUtil;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CandleBlock;
+import net.minecraft.world.level.block.CandleCakeBlock;
+import snownee.jade.addon.vanilla.StatusEffectsProvider;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.theme.IThemeHelper;
+import snownee.jade.api.ui.BoxStyle;
+import snownee.jade.api.ui.IElementHelper;
+
+public class CakeEffectsProvider implements IBlockComponentProvider {
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
+        Block block = accessor.getBlock();
+        CakeEffects cakeEffects = null;
+
+        if (block instanceof CakeEffectsHolder holder) {
+            cakeEffects = holder.getCakeEffects();
+        }
+        else if (block instanceof BaseCandleCakeBlock candleCake) {
+            cakeEffects = candleCake.getOriginalCake().getCakeEffects();
+        }
+        else if (Util.getVanillaCandleCakes().contains(block)) {
+            cakeEffects = ((CakeEffectsHolder) Blocks.CAKE).getCakeEffects();
+        }
+
+        if (cakeEffects != null) {
+            IElementHelper helper = IElementHelper.get();
+            IThemeHelper theme = IThemeHelper.get();
+            ITooltip box = helper.tooltip();
+
+            for (CakeEffects.MobEffectHolder holder : cakeEffects.mobEffects()) {
+                MobEffectInstance instance = new MobEffectInstance(holder.effect(), holder.duration().orElse(0), holder.amplifier().orElse(0));
+                Component name = StatusEffectsProvider.getEffectName(instance);
+                String duration;
+
+                if (instance.isInfiniteDuration()) {
+                    duration = I18n.get("effect.duration.infinite");
+                }
+                else {
+                    duration = StringUtil.formatTickDuration(instance.getDuration());
+                }
+
+                Component info = Component.translatable("jade.potion", name, duration);
+                box.add(holder.effect().getCategory() != MobEffectCategory.HARMFUL ? theme.success(info) : theme.danger(info));
+            }
+            tooltip.add(helper.box(box, BoxStyle.getNestedBox()));
+        }
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return JustMoreCakes.loc("cake_effects");
+    }
+}
--- common/src/main/java/einstein/jmc/compat/jade/ModJadePlugin.java
+++ common/src/main/java/einstein/jmc/compat/jade/ModJadePlugin.java
@@ -1,20 +1,20 @@
 package einstein.jmc.compat.jade;
 
 import einstein.jmc.JustMoreCakes;
+import einstein.jmc.block.CakeEffectsHolder;
 import einstein.jmc.block.cake.BaseCakeBlock;
 import einstein.jmc.block.cake.candle.BaseCandleCakeBlock;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CakeBlock;
 import net.minecraft.world.level.block.CandleCakeBlock;
-import snownee.jade.api.IWailaClientRegistration;
-import snownee.jade.api.IWailaCommonRegistration;
-import snownee.jade.api.IWailaPlugin;
-import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.*;
 
 @WailaPlugin
 public class ModJadePlugin implements IWailaPlugin {
 
     public static final CakeInfoProvider CAKE_INFO_COMPONENT_PROVIDER = new CakeInfoProvider();
+    public static final CakeEffectsProvider CAKE_EFFECT_PROVIDER = new CakeEffectsProvider();
     public static final ResourceLocation DISPLAY_TYPE = JustMoreCakes.loc("cake_info.display_type");
     public static final ResourceLocation FOOD_ICONS_PER_LINE = JustMoreCakes.loc("cake_info.food_icons_per_line");
     public static final ResourceLocation SHOW_SATURATION = JustMoreCakes.loc("cake_info.show_saturation");
@@ -34,6 +34,11 @@ public class ModJadePlugin implements IWailaPlugin {
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, BaseCandleCakeBlock.class);
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, CakeBlock.class);
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, CandleCakeBlock.class);
+
+        registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, BaseCakeBlock.class);
+        registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, BaseCandleCakeBlock.class);
+        registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, CakeBlock.class);
+        registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, CandleCakeBlock.class);
     }
 
     public enum CakeInfoDisplayType {
