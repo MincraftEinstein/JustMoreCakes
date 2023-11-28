312fd34e9a6a42fbf939ea63a2874d96b2d7e610
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-11-27 22:02:21 -0500

    Fixed Jade Display Type config not affecting saturation

--- common/src/main/java/einstein/jmc/compat/jade/providers/CakeInfoProvider.java
+++ common/src/main/java/einstein/jmc/compat/jade/providers/CakeInfoProvider.java
@@ -14,9 +14,15 @@ import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IBlockComponentProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
+import snownee.jade.api.ui.IDisplayHelper;
 import snownee.jade.api.ui.IElementHelper;
+import snownee.jade.overlay.DisplayHelper;
+
+import java.text.DecimalFormat;
 
 public class CakeInfoProvider implements IBlockComponentProvider {
+
+    private static final DecimalFormat FORMATTER = new DecimalFormat("0.##");
 
     @Override
     public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
@@ -24,21 +30,23 @@ public class CakeInfoProvider implements IBlockComponentProvider {
         if (block instanceof BaseCakeBlock cake) {
             Pair<Integer, Float> nourishment = cake.getNourishment();
             int nutrition = nourishment.getFirst();
+            int slices = cake.getBiteCount() + 1;
 
-            addNutrition(tooltip, config, cake.getBiteCount() + 1, nutrition);
-            addSaturation(tooltip, config, nutrition, nourishment.getSecond());
+            addNutrition(tooltip, config, slices, nutrition);
+            addSaturation(tooltip, config, slices, nutrition, nourishment.getSecond());
         }
         else if (block instanceof BaseCandleCakeBlock candleCake) {
             BaseCakeBlock originalCake = candleCake.getOriginalCake();
             Pair<Integer, Float> nourishment = originalCake.getNourishment();
             int nutrition = nourishment.getFirst();
+            int slices = originalCake.getBiteCount() + 1;
 
-            addNutrition(tooltip, config, originalCake.getBiteCount() + 1, nutrition);
-            addSaturation(tooltip, config, originalCake.getNourishment().getFirst(), originalCake.getNourishment().getSecond());
+            addNutrition(tooltip, config, slices, nutrition);
+            addSaturation(tooltip, config, slices, originalCake.getNourishment().getFirst(), originalCake.getNourishment().getSecond());
         }
         else if (block == Blocks.CAKE || Util.getVanillaCandleCakes().contains(block)) {
             addNutrition(tooltip, config, 7, 2);
-            addSaturation(tooltip, config, 2, 0.1F);
+            addSaturation(tooltip, config, 7, 2, 0.1F);
         }
     }
 
@@ -50,10 +58,13 @@ public class CakeInfoProvider implements IBlockComponentProvider {
         }
     }
 
-    private static void addSaturation(ITooltip tooltip, IPluginConfig config, int nutrition, float saturationModifier) {
+    private static void addSaturation(ITooltip tooltip, IPluginConfig config, int slices, int nutrition, float saturationModifier) {
         if (config.get(ModJadePlugin.SHOW_SATURATION)) {
             if (saturationModifier > 0 && nutrition > 0) {
-                tooltip.add(IElementHelper.get().text(Component.translatable("jade.plugin_jmc.saturation", nutrition * saturationModifier * 2)));
+                if (config.getEnum(ModJadePlugin.DISPLAY_TYPE) == ModJadePlugin.CakeInfoDisplayType.PER_SLICE) {
+                    saturationModifier /= slices;
+                }
+                tooltip.add(IElementHelper.get().text(Component.translatable("jade.plugin_jmc.saturation", FORMATTER.format(nutrition * saturationModifier * 2))));
             }
         }
     }
