07a1ed7cca8349d239a33a3040821c6d7ddab382
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-11-27 19:31:00 -0500

    Added Jade candle type provider

--- common/src/main/java/einstein/jmc/compat/jade/ModJadePlugin.java
+++ common/src/main/java/einstein/jmc/compat/jade/ModJadePlugin.java
@@ -4,10 +4,7 @@ import einstein.jmc.block.CakeOvenBlock;
 import einstein.jmc.block.cake.BaseCakeBlock;
 import einstein.jmc.block.cake.candle.BaseCandleCakeBlock;
 import einstein.jmc.block.entity.CakeOvenBlockEntity;
-import einstein.jmc.compat.jade.providers.CakeEffectsProvider;
-import einstein.jmc.compat.jade.providers.CakeInfoProvider;
-import einstein.jmc.compat.jade.providers.CakeOvenProvider;
-import einstein.jmc.compat.jade.providers.ItemStorageRemoverProvider;
+import einstein.jmc.compat.jade.providers.*;
 import einstein.jmc.init.ModBlocks;
 import einstein.jmc.util.Util;
 import net.minecraft.resources.ResourceLocation;
@@ -30,12 +27,14 @@ public class ModJadePlugin implements IWailaPlugin {
     public static final CakeEffectsProvider CAKE_EFFECT_PROVIDER = new CakeEffectsProvider();
     public static final CakeOvenProvider CAKE_OVEN_PROVIDER = new CakeOvenProvider();
     public static final ItemStorageRemoverProvider ITEM_STORAGE_REMOVER_PROVIDER = new ItemStorageRemoverProvider();
+    public static final CandleTypeProvider CANDLE_TYPE_PROVIDER = new CandleTypeProvider();
 
     // Features
     public static final ResourceLocation CAKE_INFO = loc("cake_info");
     public static final ResourceLocation CAKE_EFFECTS = loc("cake_effects");
     public static final ResourceLocation CAKE_OVEN = loc("cake_oven");
     public static final ResourceLocation ITEM_STORAGE_REMOVER = loc("cake_oven.item_storage_remover");
+    public static final ResourceLocation CANDLE_TYPE = loc("candle_type");
 
     // Configs
     public static final ResourceLocation DISPLAY_TYPE = loc("cake_info.display_type");
@@ -61,6 +60,9 @@ public class ModJadePlugin implements IWailaPlugin {
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, BaseCandleCakeBlock.class);
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, CakeBlock.class);
         registration.registerBlockComponent(CAKE_INFO_COMPONENT_PROVIDER, CandleCakeBlock.class);
+
+        registration.registerBlockComponent(CANDLE_TYPE_PROVIDER, BaseCandleCakeBlock.class);
+        registration.registerBlockComponent(CANDLE_TYPE_PROVIDER, CandleCakeBlock.class);
 
         registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, BaseCakeBlock.class);
         registration.registerBlockComponent(CAKE_EFFECT_PROVIDER, BaseCandleCakeBlock.class);
@@ -99,6 +101,7 @@ public class ModJadePlugin implements IWailaPlugin {
         registration.markAsClientFeature(SHOW_NUTRITION);
         registration.markAsClientFeature(SHOW_SATURATION);
         registration.markAsClientFeature(HIDE_TRAPPED_CAKES);
+        registration.markAsClientFeature(CANDLE_TYPE);
     }
 
     private static boolean isTrappedCandleCake(Block block, BaseCakeBlock cake) {
