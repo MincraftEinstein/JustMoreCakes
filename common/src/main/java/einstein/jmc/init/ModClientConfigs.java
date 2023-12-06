bc3cd1f515c3404c0f2eb95854efdb313d937581
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-12-06 18:48:41 -0500

    Fixed incorrect config translations
    Also cleaned up ModClientConfigs

--- common/src/main/java/einstein/jmc/init/ModClientConfigs.java
+++ common/src/main/java/einstein/jmc/init/ModClientConfigs.java
@@ -6,17 +6,25 @@ import static einstein.jmc.init.ModCommonConfigs.key;
 
 public class ModClientConfigs {
 
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder().comment("Particle effects emitted from cakes").push("Cake particles");
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    static {
+        BUILDER.comment("Particle effects emitted from cakes").push("Cake Particles");
+    }
 
     public static final ForgeConfigSpec.BooleanValue REDSTONE_CAKE_PARTICLES = BUILDER
-            .translation(key("config.jmc.redstone_cake_particles"))
+            .translation(key("cake_particles.redstone_cake_particles"))
             .define("redstoneCakeParticles", true);
     public static final ForgeConfigSpec.BooleanValue ENDER_CAKE_PARTICLES = BUILDER
-            .translation(key("config.jmc.ender_cake_particles"))
+            .translation(key("cake_particles.ender_cake_particles"))
             .define("enderCakeParticles", true);
     public static final ForgeConfigSpec.BooleanValue LAVA_CAKE_PARTICLES = BUILDER
-            .translation(key("lava_cake_particles"))
+            .translation(key("cake_particles.lava_cake_particles"))
             .define("lavaCakeParticles", false);
+
+    static {
+        BUILDER.pop();
+    }
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 }
