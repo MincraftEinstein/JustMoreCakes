59fad015b36a3dbd4fdd8b67ef16ead75845c1b8
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-02-24 19:03:14 -0500

    Rewrote Block mixins

--- fabric/src/main/java/einstein/jmc/mixin/BlockMixin.java
+++ fabric/src/main/java/einstein/jmc/mixin/BlockMixin.java
@@ -1,5 +1,7 @@
 package einstein.jmc.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import einstein.jmc.data.packs.ModBlockTags;
 import einstein.jmc.init.ModItems;
 import net.minecraft.core.BlockPos;
@@ -11,20 +13,18 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static net.minecraft.world.level.block.Block.dropResources;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
 
-    @Redirect(method = "playerDestroy", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V"))
-    private void playerDestroy(BlockState state, Level level, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack) {
+    @WrapOperation(method = "playerDestroy", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V"))
+    private void playerDestroy(BlockState state, Level level, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack, Operation<Void> original) {
         if (stack.is(ModItems.CAKE_SPATULA.get()) && state.is(ModBlockTags.CAKE_SPATULA_USABLE)) {
             dropResources(state, level, pos, blockEntity, entity, ItemStack.EMPTY);
+            return;
         }
-        else {
-            dropResources(state, level, pos, blockEntity, entity, stack);
-        }
+        original.call(state, level, pos, blockEntity, entity, stack);
     }
 }
