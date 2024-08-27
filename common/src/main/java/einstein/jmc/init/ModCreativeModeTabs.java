5e8c4ec78269c2b433ef53963f73efd3f5f390b0
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-08-27 16:10:48 -0400

    Mod creative tab only displays enabled items now

--- common/src/main/java/einstein/jmc/init/ModCreativeModeTabs.java
+++ common/src/main/java/einstein/jmc/init/ModCreativeModeTabs.java
@@ -4,6 +4,7 @@ import einstein.jmc.platform.Services;
 import einstein.jmc.platform.services.RegistryHelper;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Supplier;
@@ -11,9 +12,17 @@ import java.util.function.Supplier;
 public class ModCreativeModeTabs {
 
     public static final Supplier<CreativeModeTab> JMC_TAB = Services.REGISTRY.registerCreativeModeTab("jmc_tab", builder ->
-            builder.icon(() -> new ItemStack(ModBlocks.CHOCOLATE_CAKE_FAMILY.getBaseCake().get())).title(Component.translatable("itemGroup.jmc.jmc_tab"))
+            builder.icon(() -> new ItemStack(ModBlocks.CHOCOLATE_CAKE_FAMILY.getBaseCake().get()))
+                    .title(Component.translatable("itemGroup.jmc.jmc_tab"))
                     .displayItems((displayParameters, output) ->
-                            RegistryHelper.CREATIVE_TAB_ITEMS.forEach(cake -> output.accept(cake.get()))).build());
+                            RegistryHelper.CREATIVE_TAB_ITEMS.forEach(supplier -> {
+                                Item item = supplier.get();
+                                if (item.isEnabled(displayParameters.enabledFeatures())) {
+                                    output.accept(item);
+                                }
+                            })
+                    ).build()
+    );
 
     public static void init() {
     }
