1eed0d7f896d278a1e493e283495239b2334bdc9
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2024-08-28 14:33:48 -0400

    Added tooltip to cake slices for when FD isn't installed

--- common/src/main/java/einstein/jmc/item/CakeSliceItem.java
+++ common/src/main/java/einstein/jmc/item/CakeSliceItem.java
@@ -1,14 +1,20 @@
 package einstein.jmc.item;
 
+import einstein.jmc.JustMoreCakes;
 import einstein.jmc.block.cake.BaseCakeBlock;
 import einstein.jmc.data.effects.CakeEffects;
+import einstein.jmc.platform.Services;
 import einstein.jmc.registration.family.CakeFamily;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class CakeSliceItem extends Item {
@@ -19,6 +25,14 @@ public class CakeSliceItem extends Item {
     public CakeSliceItem(Properties properties, CakeFamily family) {
         super(properties);
         this.family = family;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, context, tooltip, flag);
+        if (!Services.PLATFORM.isModLoaded(JustMoreCakes.FARMERS_DELIGHT_MOD_ID)) {
+            tooltip.add(Component.translatable("item.jmc.fd_support.cake_slice.info").withStyle(ChatFormatting.RED));
+        }
     }
 
     @Override
